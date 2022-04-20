d4ad48c56f3a5cbe5afcfc3c58e175847cb5025f
rortizs <rortizs@miumg.edu.gt>
2022-04-20 14:23:01 -0600

    Add vehicle types Controller, View

---
+++ Vehicles.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //las tablas deben tener una propiedad DbSet
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+        //sobre escribir el metodo 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+            //expresion lamba para indicar como unico la descripcion de la entidad
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}

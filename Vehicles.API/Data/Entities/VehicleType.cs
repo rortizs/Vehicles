5d1d648f610014625abd1bfa31f66cae7fbdadda
rortizs <rortizs@miumg.edu.gt>
2022-04-20 13:03:18 -0600

    Primera parta funcional

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+
+        public int Id { get; set; }
+        [Display(Name ="Tipo de Vehiculo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio" )]
+        public string Description { get; set; }
+    }
+}
